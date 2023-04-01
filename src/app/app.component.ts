22cabbe190278adf686835394c9b829df5e1ef38
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-01 15:04:13 -0500

    Iniciando proyecto y configurando primeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Alonso anChanTe';
+
+  mostrarNombre = () => {
+    console.log(this.nombre);
+  };
 }
