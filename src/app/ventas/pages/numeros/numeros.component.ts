59a520f4c2886906efc7026040d2f3f9296d05d8
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-03 09:53:13 -0500

    DecimalPipe y PercentePipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumerosComponent {
-
+  ventasNetas: number = 2567855.5761;
+  porcentaje: number = 0.4855;
 }
