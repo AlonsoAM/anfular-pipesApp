c5f58cfa5e341a3d659adb2f710a4eeb877de9fb
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-07 22:14:06 -0500

    agregando pipes personalizados

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mayusculas',
+})
+export class MayusculasPipe implements PipeTransform {
+  transform(value: string, ...args: any[]): string {
+    // console.log(value);
+    return value.toUpperCase();
+  }
+}
